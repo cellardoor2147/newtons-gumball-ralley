d1fbe2713afcf4e482006000fd8a5de13ed395b5
Michael Renda <micoo227@users.noreply.github.com>
2021-03-10 19:22:27 -0500

    Implemented functionality for kill obstacle
    - tagged ball as "Player"
    - destroy ball GameObject when it contacts kill obstacle

---
+++ Newton's Gumball Ralley/Assets/Scripts/KillObstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillObstacle : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            Destroy(other.gameObject);
+    }
+}
