c6d32f58e9554486b4faadf30de32a0c69f14f4e
Michael Renda <micoo227@users.noreply.github.com>
2021-03-11 12:51:04 -0500

    Addressed nits
    - added Obstacle namespace
    - created Obstacle folder within Scripts folder
    - placed KillObstacle.cs in Obstacle folder

---
+++ Newton's Gumball Ralley/Assets/Scripts/Obstacle/KillObstacle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Obstacle
+{
+    public class KillObstacle : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+                Destroy(other.gameObject);
+        }
+    }
+}
