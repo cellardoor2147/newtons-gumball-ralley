ce8c6c3e6f80bfbdae2d29f9c46d2b09dcc28f60
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-03-11 17:55:21 -0800

    Added scalable play mode gui (#4)
    * Added scalable play mode gui
    
    - Added gui manager to maintain gui state
    - Added gui controller to denote gui type and deal with future gui type specific functionality
    - Imported placeholder assets
    - Added function-less level counter, play/pause/restart buttons, and start counter
    
    * Updated sorting order to ensure gui is rendered above the ball
    
    * Added placeholder font for GUI

---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/GUIController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUI
+{
+    public class GUIController : MonoBehaviour
+    {
+        public GUIType guiType;
+    }
+}
---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GUI
+{
+    public enum GUIType
+    {
+        MainMenu = 0,
+        PlayMode = 1,
+        PauseMenu = 2
+    }
+
+    public class GUIManager : MonoBehaviour
+    {
+        List<GUIController> guiControllers;
+
+        private void Awake()
+        {
+            guiControllers = GetComponentsInChildren<GUIController>().ToList();
+        }
+
+        public void SetActiveGUI(GUIType guiType)
+        {
+            SetAllGUIToInactive();
+            GUIController guiControllerToActivate =
+                guiControllers.Find(guiController => guiController.guiType.Equals(guiType));
+            if (guiControllerToActivate != null)
+            {
+                guiControllerToActivate.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError($"Tried setting invalid GUI type: {guiType.ToString()}");
+            }
+        }
+
+        private void SetAllGUIToInactive()
+        {
+            guiControllers.ForEach(guiController => guiController.gameObject.SetActive(false));
+        }
+    }
+}
