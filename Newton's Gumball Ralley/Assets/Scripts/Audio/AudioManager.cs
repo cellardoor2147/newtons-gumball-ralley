6edce25c7f688ee21f5fe0c461163150c58e74c6
Dale Ross <dkross@wesleyan.edu>
2021-03-11 20:49:40 -0500

    Audio Manager Prefab and Updates
    Made AudioManager Prefab
    Adjusted AudioManager Framework
    Made MenuManager.cs which may serve as a GUI manager, also might merge with gamestate manager

--- Newton's Gumball Ralley/Assets/Scripts/Audio/AudioManager.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Audio/AudioManager.cs
@@ -10,17 +10,24 @@ public class Sound {
     [Range(0.5f, 1f)]
     public float pitch = 1f;
 
+    public bool loop = false;
+
     private AudioSource source; 
 
     public void SetSource (AudioSource _source){
         source = _source;
         source.clip = clip;
+        source.loop = loop;
     }
 
     public void Play (){
         source.pitch = pitch;
         source.volume = volume; 
         source.Play();
+    }
+    public void Stop()
+    {
+        source.Stop();
     }
 }
 public class AudioManager : MonoBehaviour
@@ -31,9 +38,12 @@ public class AudioManager : MonoBehaviour
     void Awake()
     {
         if (instance != null) {
-            Debug.LogError("More than one AudioManager in the Scene");
+            if (instance != this){
+                Destroy(this.gameObject);
+            }
         } else {
             instance = this;
+            DontDestroyOnLoad(this);
         }
     }
 
@@ -44,7 +54,13 @@ public class AudioManager : MonoBehaviour
             _go.transform.SetParent (this.transform);
             sounds[i].SetSource (_go.AddComponent<AudioSource>());
         }
+        PlaySound ("Main_Menu_Music"); 
     }
+    // To be implemented when GUI/gamestate manager is added
+    
+    // void Update () {
+    //     if (time.time >5f)
+    // }
 
     public void PlaySound (string _name) {
         for (int i = 0; i < sounds.Length; i++)
@@ -52,6 +68,20 @@ public class AudioManager : MonoBehaviour
             if (sounds[i].name == _name)
             {
                 sounds[i].Play();
+                return;
+            }
+        }
+        //no sound with _name
+        Debug.LogWarning("AudioManager: Sound not found in list " + _name);
+    }
+
+    public void StopSound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Stop();
                 return;
             }
         }
---
+++ Newton's Gumball Ralley/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] string hoverOverSound = "ButtonHover";
+
+    [SerializeField] string pressButtonSound = "ButtonPress";
+    AudioManager audioManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null) {
+            Debug.LogError ("No audiomanager found");
+        }
+    }
+    // To be implemented when GUI/gamestate manager is added
+    
+    // public void OnMouseOver () {
+    //     audioManager.PlaySound(hoverOverSound);
+    // }
+
+
+}
