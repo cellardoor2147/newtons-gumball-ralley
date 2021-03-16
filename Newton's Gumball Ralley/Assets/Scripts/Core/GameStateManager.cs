5150a318f76bc1c5d683cde944230ce419d7dcbe
Joshua Reed <jtreed@wesleyan.edu>
2021-03-16 15:27:53 -0700

    Added main menu, pause menu
    - added screen scaling to main menu and pause menu
    - added functional buttons to main/pause menus
    - implemented game state manager

---
+++ Newton's Gumball Ralley/Assets/Scripts/Core/GameStateManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using GUI;
+
+namespace Core
+{
+    public enum GameState
+    {
+        MainMenu = 0,
+        Playing = 1,
+        Paused = 2
+    }
+
+    public class GameStateManager : MonoBehaviour
+    {
+        private readonly static string MAIN_MENU_SCENE_KEY = "Main Menu";
+        private readonly static string GAME_SCENE_KEY = "Game";
+
+        private static GameStateManager instance;
+
+        private GameState gameState;
+
+        private GameStateManager() {} // Prevents instantiation outside of this class
+
+        private void Awake()
+        {
+            SetInstance();
+            PreventManagersContainerFromBeingDestroyedOnLoad();
+        }
+
+        private void SetInstance()
+        {
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
+        }
+
+        private void PreventManagersContainerFromBeingDestroyedOnLoad()
+        {
+            DontDestroyOnLoad(transform.parent.gameObject);
+        }
+
+        private void Start()
+        {
+            SetGameState(GameState.MainMenu);
+        }
+
+        public static GameState GetGameState()
+        {
+            return instance.gameState;
+        }
+
+        public static void SetGameState(GameState gameState)
+        {
+            switch (gameState)
+            {
+                case GameState.MainMenu:
+                    Time.timeScale = 0.0f;
+                    LoadScene(MAIN_MENU_SCENE_KEY);
+                    GUIManager.SetActiveGUI(GUIType.MainMenu);
+                    break;
+                case GameState.Playing:
+                    Time.timeScale = 1.0f;
+                    LoadScene(GAME_SCENE_KEY);
+                    GUIManager.SetActiveGUI(GUIType.PlayMode);
+                    break;
+                case GameState.Paused:
+                    Time.timeScale = 0.0f;
+                    LoadScene(GAME_SCENE_KEY);
+                    GUIManager.SetActiveGUI(GUIType.SettingsMenu);
+                    break;
+                default:
+                    Debug.Log($"Tried setting invalid game state: {gameState}");
+                    break;
+            }
+            instance.gameState = gameState;
+        }
+
+        private static void LoadScene(string sceneName)
+        {
+            if (SceneManager.GetActiveScene().name.Equals(sceneName))
+            {
+                return;
+            }
+            SceneManager.LoadScene(sceneName);
+        }
+
+        private void Update()
+        {
+            bool shouldOpenSettingsMenuByKeyPress =
+                gameState.Equals(GameState.Playing) &&
+                (Input.GetKeyDown(KeyCode.P) ||
+                Input.GetKeyDown(KeyCode.Escape));
+            if (shouldOpenSettingsMenuByKeyPress)
+            {
+                SetGameState(GameState.Paused);
+                return;
+            }
+            bool shouldCloseSettingsMenuByKeyPress =
+                gameState.Equals(GameState.Paused) &&
+                (Input.GetKeyDown(KeyCode.P) ||
+                Input.GetKeyDown(KeyCode.Escape));
+            if (shouldCloseSettingsMenuByKeyPress)
+            {
+                SetGameState(GameState.Playing);
+                return;
+            }
+        }
+    }
+}
--- Newton's Gumball Ralley/Assets/Scripts/GUI/GUIManager.cs
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/GUIManager.cs
@@ -8,36 +8,65 @@ namespace GUI
     {
         MainMenu = 0,
         PlayMode = 1,
-        PauseMenu = 2
+        SettingsMenu = 2
     }
 
     public class GUIManager : MonoBehaviour
     {
-        List<GUIController> guiControllers;
+        private readonly static string CANVAS_KEY = "Canvas";
+        
+        private static GUIManager instance;
+
+        private List<GUIController> guiControllers;
+
+        private GUIManager() {} // Prevents instantiation outside of this class
 
         private void Awake()
         {
-            guiControllers = GetComponentsInChildren<GUIController>().ToList();
+            SetInstance();
+            AttachMainCameraToCanvas();
+            guiControllers = GetComponentsInChildren<GUIController>(true).ToList();
         }
 
-        public void SetActiveGUI(GUIType guiType)
+        private void SetInstance()
         {
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
+        }
+
+        private void AttachMainCameraToCanvas()
+        {
+            transform.Find(CANVAS_KEY).GetComponent<Canvas>().worldCamera = Camera.main;
+        }
+
+        public static void SetActiveGUI(GUIType guiType)
+        {
+            Debug.Log(guiType);
             SetAllGUIToInactive();
-            GUIController guiControllerToActivate =
-                guiControllers.Find(guiController => guiController.guiType.Equals(guiType));
+            GUIController guiControllerToActivate = instance.guiControllers.Find(
+                guiController => guiController.guiType.Equals(guiType)
+            );
             if (guiControllerToActivate != null)
             {
                 guiControllerToActivate.gameObject.SetActive(true);
             }
             else
             {
-                Debug.LogError($"Tried setting invalid GUI type: {guiType.ToString()}");
+                Debug.LogError($"Tried setting invalid GUI type: {guiType}");
             }
         }
 
-        private void SetAllGUIToInactive()
+        private static void SetAllGUIToInactive()
         {
-            guiControllers.ForEach(guiController => guiController.gameObject.SetActive(false));
+            instance.guiControllers.ForEach(
+                guiController => guiController.gameObject.SetActive(false)
+            );
         }
     }
 }
