8df2cbc4682b6e0cdb48aa73fbf3f7dc317ca784
Michael Renda <micoo227@users.noreply.github.com>
2021-03-21 18:49:55 -0400

    Implemented screw simple machine
    - Added SimpleMachine tag to both inclined planes
    - Adjusted mass of inclined planes to be greater than before
    - Created ScrewBehavior Script in new Simple Machines script folder
    - Added Screw prefab to Simple Machine prefab folder

---
+++ Newton's Gumball Ralley/Assets/Scripts/Simple Machines/ScrewBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Screw
+{
+    public class ScrewBehavior : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                if (collision.gameObject.CompareTag("SimpleMachine"))
+                {
+                    collision.gameObject.AddComponent<HingeJoint2D>();
+                    transform.SetParent(collision.transform, true);
+                    collision.gameObject.GetComponent<HingeJoint2D>().anchor = gameObject.transform.localPosition;
+                    collision.gameObject.GetComponent<HingeJoint2D>().enableCollision = true;
+                }
+            }
+        }
+    }
+}
