5efceae3150a2a98b0a4a3abc56d6c78a2c6508a
Dale Ross <dkross@wesleyan.edu>
2021-03-21 14:45:37 -0400

    Update EditManager.cs
    Small fixes

--- Newton's Gumball Ralley/Assets/Scripts/EditMode/EditManager.cs
+++ Newton's Gumball Ralley/Assets/Scripts/EditMode/EditManager.cs
@@ -6,17 +6,13 @@ public class EditManager : MonoBehaviour
 
     private MachineTypeSO activeMachineType;
 
-    private bool ableToSpawnMachine;
-    private bool shouldRespondToMouseDown;
-
     // Update is called once per frame
     private void Update()
     {
-        shouldRespondToMouseDown = Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject();
+        bool shouldRespondToMouseDown = Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject();
         if (shouldRespondToMouseDown) {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            ableToSpawnMachine = CanSpawnMachine(); 
-            if (ableToSpawnMachine) {
+            if (CanSpawnMachine()) {
                 Instantiate(activeMachineType.prefab, mousePosition, Quaternion.identity);
             }
         }
@@ -34,10 +30,7 @@ public class EditManager : MonoBehaviour
         Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         BoxCollider2D machineCollider = activeMachineType.prefab.GetComponent<BoxCollider2D>();
 
-        if (Physics2D.OverlapBox(position + machineCollider.offset, machineCollider.size, 0) != null) {
-            return false;
-        } else {
-            return true;
-        }
+        bool ableToSpawnMachine = Physics2D.OverlapBox(position + machineCollider.offset, machineCollider.size, 0) == null;
+        return ableToSpawnMachine;
     }
 }
