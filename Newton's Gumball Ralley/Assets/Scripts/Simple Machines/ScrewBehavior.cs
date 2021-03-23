9ed3efc54fd6d4fbcdd40c0ce3f5dd96867ab4a8
Michael Renda <micoo227@users.noreply.github.com>
2021-03-22 20:34:06 -0400

    Made requested corrections
    - Changed coordinates of screw and inclined planes 1 & 2 to (0,0,0)
    - Cleaned up ScrewBehavior script
    - Made Screw collider disable if not colliding with simple machine upon start

--- Newton's Gumball Ralley/Assets/Scripts/Simple Machines/ScrewBehavior.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Simple Machines/ScrewBehavior.cs
@@ -4,17 +4,30 @@ namespace Screw
 {
     public class ScrewBehavior : MonoBehaviour
     {
+        private bool collidedWithSimpleMachine = false;
+        private Collider2D screwCollider;
+
+        void Start()
+        {
+            screwCollider = GetComponent<Collider2D>();
+        }
+        
+        void Update()
+        {
+            if (!collidedWithSimpleMachine)
+                screwCollider.enabled = false;
+        }
+
         void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            collidedWithSimpleMachine = collision.gameObject.GetComponent<Rigidbody2D>() != null
+                && collision.gameObject.CompareTag("SimpleMachine");
+            if (collidedWithSimpleMachine)
             {
-                if (collision.gameObject.CompareTag("SimpleMachine"))
-                {
-                    collision.gameObject.AddComponent<HingeJoint2D>();
-                    transform.SetParent(collision.transform, true);
-                    collision.gameObject.GetComponent<HingeJoint2D>().anchor = gameObject.transform.localPosition;
-                    collision.gameObject.GetComponent<HingeJoint2D>().enableCollision = true;
-                }
+                collision.gameObject.AddComponent<HingeJoint2D>();
+                transform.SetParent(collision.transform, true);
+                collision.gameObject.GetComponent<HingeJoint2D>().anchor = gameObject.transform.localPosition;
+                collision.gameObject.GetComponent<HingeJoint2D>().enableCollision = true;
             }
         }
     }
