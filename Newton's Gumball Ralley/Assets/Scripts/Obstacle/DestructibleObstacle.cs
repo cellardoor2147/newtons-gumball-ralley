5e5e9b7a4e87b6ce3ce96b5b08d092224a442eef
Michael Renda <micoo227@users.noreply.github.com>
2021-04-05 20:10:23 -0400

    Added destructible object and associated functionality
    - Created DestructibleObject prefab which spawns a split obstacle if wedge hits it with enough velocity
    - SplitDestructibleObject spawns at location of wedge-obstacle collision
    - Created "IDestructible" interface which provides functionality for objects that should split

---
+++ Newton's Gumball Ralley/Assets/Scripts/Obstacle/DestructibleObstacle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DestructibleObject
+{
+    public class DestructibleObstacle : MonoBehaviour, IDestructible
+    {
+        [SerializeField] float breakSpeed;
+        [SerializeField] UnityEngine.Object destructibleRef;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            float localYVel = collision.transform.InverseTransformDirection(collision.rigidbody.velocity).y;
+            if (collision.collider.name == "Wedge" && Mathf.Abs(localYVel) > breakSpeed)
+            {
+                Split(collision.transform);
+            }
+        }
+
+        public void Split(Transform hitTransform)
+        {
+            GameObject destructible = Instantiate(destructibleRef, hitTransform.position, hitTransform.rotation) as GameObject;
+            destructible.transform.localScale = transform.lossyScale;
+            Destroy(this.gameObject);
+        }
+    }
+}
