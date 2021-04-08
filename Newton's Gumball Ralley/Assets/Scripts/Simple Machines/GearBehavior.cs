f1f0a36c054fa5ee36b4dea65e90880d01620dc9
Dale Ross <dkross@wesleyan.edu>
2021-04-07 21:59:33 -0400

    Updated SOs

--- Newton's Gumball Ralley/Assets/Scripts/Simple Machines/GearBehavior.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Simple Machines/GearBehavior.cs
@@ -4,11 +4,13 @@ using Core;
 namespace Gear {
     public class GearBehavior : MonoBehaviour
     {
+        private Rigidbody2D rigidbody2D;
 
-
-
-
-
+        private void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            
+        }
     }
 }
 
