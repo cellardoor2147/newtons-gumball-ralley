c556f7fee3b9a92aae69f8da547d85ce60e36fd7
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-04-09 23:40:48 -0700

    Added rotation to incline planes (#19)
    * Got basic functionality for rotation working
    
    * Fixed two bugs
    
    one that left rotation arrows behind even if the incline plane was deleted, and another that prevents the edit mode GUI from appearing if a simple machine is deleted while dragging
    
    * Prevent horizontal/vertical rotation and bug fixes
    
    * Prevent rotating into a collision
    
    * Revert "Fixed Bug caused by merge conflict" (#21)
    
    This reverts commit 3623d8a97d319637e9b9a02ad7149905332fa3a9.
    
    * Revert "Dale add audio s os" (#22)
    
    * Revert "Merge branch 'main' into josh-add-rotation-to-incline-plane"
    
    This reverts commit 8886b874159198c2ece05ef1746a2c868e317b33, reversing
    changes made to 5ce01160a4144953af65e52c85be52083d4aa2bc.

--- Newton's Gumball Ralley/Assets/Scripts/Simple Machines/DraggingController.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Simple Machines/DraggingController.cs
@@ -10,6 +10,10 @@ namespace SimpleMachine
     public class DraggingController : MonoBehaviour
     {
         private static readonly string PLACED_OBJECTS_KEY = "Placed Objects";
+        private static readonly string SIMPLE_MACHINE_TAG = "SimpleMachine";
+        private static readonly string BALL_TAG = "Player";
+
+        [SerializeField] private GameObject rotationArrowsPrefab;
 
         private bool hasBeenPlaced;
         private Collider2D collider2D;
@@ -19,6 +23,8 @@ namespace SimpleMachine
         private Rigidbody2D rigidbody2D;
         private Vector2 lastValidPosition;
         private Quaternion lastValidRotation;
+        private GameObject rotationArrows;
+        private GameObject placedObjectsContainer;
         private PlacedObjectManager objectManager;
         private PlacedObjectMetaData objectMetaData;
         [SerializeField] private PlacedObjectMetaData leverPlatformMetaData;
@@ -36,6 +42,7 @@ namespace SimpleMachine
             rigidbody2D = GetComponent<Rigidbody2D>();
             lastValidPosition = transform.position;
             lastValidRotation = transform.rotation;
+            placedObjectsContainer = GameObject.Find(PLACED_OBJECTS_KEY);
             objectManager = GetComponent<PlacedObjectManager>();
             objectMetaData = objectManager.metaData;
         }
@@ -75,6 +82,7 @@ namespace SimpleMachine
             {
                 return;
             }
+            RemoveRotationArrows();
             transform.position = GetMousePositionInWorldCoordinates();
             if (ShouldPreventObjectFromBeingPlaced())
             {
@@ -104,6 +112,8 @@ namespace SimpleMachine
                 if (!hasBeenPlaced)
                 {
                     Destroy(gameObject);
+                    EditModeManager.ShowEditModeGUI();
+                    return;
                 }
                 ResetTransform();
             }
@@ -118,6 +128,7 @@ namespace SimpleMachine
             }
             collider2D.isTrigger = colliderIsTriggerByDefault;
             spriteRenderer.color = defaultColor;
+            AddRotationArrows();
             EditModeManager.ShowEditModeGUI();
         }
 
@@ -130,6 +141,8 @@ namespace SimpleMachine
             bool playerRightClickedThisObject = Input.GetMouseButtonDown(1);
             if (playerRightClickedThisObject)
             {
+                RemoveRotationArrows();
+                EditModeManager.ShowEditModeGUI();
                 Destroy(gameObject);
             }
         }
@@ -250,6 +263,90 @@ namespace SimpleMachine
         public void RevertFromGray()
         {
             spriteRenderer.color = defaultColor;
+        }
+
+        public void RemoveRotationArrows()
+        {
+            if (rotationArrows != null)
+            {
+                Destroy(rotationArrows);
+            }
+        }
+
+        public void AddRotationArrows()
+        {
+            if (rotationArrowsPrefab != null)
+            {
+                rotationArrows = Instantiate(
+                    rotationArrowsPrefab,
+                    transform.position,
+                    Quaternion.identity,
+                    placedObjectsContainer.transform
+                );
+                foreach (
+                    RotatingController child in rotationArrows.GetComponentsInChildren<RotatingController>()
+                )
+                {
+                    child.SetObjectToRotate(gameObject);
+                }
+            }
+        }
+
+        public void Rotate(float rotationMagnitude)
+        {
+            RemoveRotationArrows();
+            RotateToNextValidPosition(rotationMagnitude);
+            AddRotationArrows();
+        }
+
+        private void RotateToNextValidPosition(float rotationMagnitude)
+        {
+            RaycastHit2D[] hits;
+            float totalRotationMagnitude = 0;
+            do
+            {
+                totalRotationMagnitude +=
+                    GetNextPotentiallyValidRotation(rotationMagnitude);
+                hits = Physics2D.BoxCastAll(
+                    transform.position,
+                    ((BoxCollider2D)collider2D).size,
+                    transform.rotation.eulerAngles.z + totalRotationMagnitude,
+                    Vector2.zero
+                );
+            } while (
+                HitsContainCollisionOtherThanSelf(hits)
+                && totalRotationMagnitude < 360f
+                && totalRotationMagnitude > -360f
+            );
+            transform.Rotate(new Vector3(0f, 0f, totalRotationMagnitude));
+            lastValidRotation = transform.rotation;
+        }
+        
+        private float GetNextPotentiallyValidRotation(float rotationMagnitude)
+        {
+            bool rotationWouldMakeGameObjectHorizontalOrVertical =
+                (Mathf.RoundToInt(transform.rotation.eulerAngles.z + rotationMagnitude) % 90) == 0;
+            if (rotationWouldMakeGameObjectHorizontalOrVertical)
+            {
+                rotationMagnitude *= 2;
+            }
+            return rotationMagnitude;
+        }
+
+        private bool HitsContainCollisionOtherThanSelf(RaycastHit2D[] hits)
+        {
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (
+                    hit.collider.gameObject != gameObject 
+                    && (hit.collider.gameObject.CompareTag(SIMPLE_MACHINE_TAG)
+                    || hit.collider.gameObject.CompareTag(BALL_TAG))
+                )
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
