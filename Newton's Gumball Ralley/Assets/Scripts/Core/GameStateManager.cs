8432a2f6f89b0027990593bf55057270162832a3
Dale Ross <dkross@wesleyan.edu>
2021-04-19 10:59:00 -0400

    Level Data Update

--- Newton's Gumball Ralley/Assets/Scripts/Core/GameStateManager.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Core/GameStateManager.cs
@@ -304,12 +304,10 @@ namespace Core
                     }
                     bool collider2IsLargeAxle =
                         collider2.gameObject.name.Equals("LargeAxle(Clone)");
-                    Debug.Log(collider2IsLargeAxle + " " + collider2.gameObject.name);
                     bool collider2IsSmallAxle =
                         collider2.gameObject.name.Equals("SmallAxle(Clone)");
                     bool collider1IsGear3 =
                         collider1.gameObject.name.Equals("Gear3(Clone)");
-                    Debug.Log(collider1IsGear3 + " " + collider1.gameObject.name);
                     bool collider1IsGear1orWheel =
                         collider1.gameObject.name.Equals("Gear1(Clone)") || collider1.gameObject.name.Equals("Wheel(Clone)");
                     if ((collider2IsLargeAxle && !collider1IsGear3) 
