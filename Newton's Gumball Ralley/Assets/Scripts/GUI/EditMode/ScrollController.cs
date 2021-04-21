c7044a2256a66db7a9d90f7707e169c7b43d3f8d
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-04-20 19:46:37 -0700

    Finished implementing scrolling functionality

---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/EditMode/ScrollController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GUI.EditMode
+{
+    public enum ScrollDirection
+    {
+        Left = 0,
+        Right = 1
+    }
+
+    public class ScrollController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private static readonly string TOOLBAR_CONTENT_CONTAINER_KEY = "Toolbar Content Container";
+
+        [SerializeField] private ScrollDirection scrollDirection;
+
+        private bool shouldScroll;
+        private ToolbarManager toolbarManager;
+
+        public void StopScrolling()
+        {
+            shouldScroll = false;
+        }
+
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+            shouldScroll = true;
+            toolbarManager = transform
+                .parent
+                .Find(TOOLBAR_CONTENT_CONTAINER_KEY)
+                .GetComponent<ToolbarManager>();
+        }
+
+        public void OnPointerExit(PointerEventData pointerEventData)
+        {
+            shouldScroll = false;
+        }
+
+        private void Update()
+        {
+            if (!shouldScroll)
+            {
+                return;
+            }
+            if (scrollDirection.Equals(ScrollDirection.Left))
+            {
+                toolbarManager.HandleScrollLeft();
+            }
+            else
+            {
+                toolbarManager.HandleScrollRight();
+            }
+        }
+    }
+}
