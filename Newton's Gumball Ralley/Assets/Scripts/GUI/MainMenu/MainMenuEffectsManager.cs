965c7cf80b5ed73fdea0e5ee16070d1162d31a4c
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-04-25 14:17:16 -0700

    Added randomly falling gumballs to main menu background

--- Newton's Gumball Ralley/Assets/Scripts/GUI/MainMenu/MainMenuEffectsManager.cs
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/MainMenu/MainMenuEffectsManager.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GUI.MainMenu
 {
     public class MainMenuEffectsManager : MonoBehaviour
     {
+        private static readonly string GUMBALL_CONTAINER_KEY = "Gumball Container";
         private static readonly string MAX_IMAGE_KEY = "Max Image";
         private static readonly string QUINN_IMAGE_KEY = "Quinn Image";
 
+        [SerializeField] private GameObject guiGumballPrefab;
+        [SerializeField] private float gumballSpawnDelay;
+        [SerializeField] private float gumballFallSpeed;
+        [SerializeField] private float gumballImageSize;
+        [SerializeField] private List<Sprite> gumballSprites;
+
+        private GameObject gumballContainer;
+        private float spawnedGumballStartX;
+        private float spawnedGumballEndX;
+        private float spawnedGumballStartY;
+        private float spawnedGumballEndY;
         private RectTransform maxTransform;
         private RectTransform quinnTransform;
         private float maxTransformStartY;
@@ -17,6 +31,13 @@ namespace GUI.MainMenu
 
         private void Awake()
         {
+            gumballContainer = transform.Find(GUMBALL_CONTAINER_KEY).gameObject;
+            Vector2 gumballContainerSize =
+                gumballContainer.GetComponent<RectTransform>().rect.size;
+            spawnedGumballStartX = -gumballContainerSize.x;
+            spawnedGumballEndX = gumballContainerSize.x;
+            spawnedGumballStartY = (gumballContainerSize.y / 2) + (gumballImageSize / 2);
+            spawnedGumballEndY = -spawnedGumballStartY;
             maxTransform = transform.Find(MAX_IMAGE_KEY).GetComponent<RectTransform>();
             quinnTransform = transform.Find(QUINN_IMAGE_KEY).GetComponent<RectTransform>();
             maxTransformStartY = -(maxTransform.rect.height / 2f);
@@ -27,12 +48,68 @@ namespace GUI.MainMenu
 
         private void OnEnable()
         {
+            StartCoroutine(RandomlySpawnGumballsInBackground());
             StartCoroutine(RaiseMaxAndQuinnTransforms());
         }
 
         private void OnDisable()
         {
+            StopAllCoroutines();
             ResetMaxAndQuinnTransforms();
+        }
+
+        private IEnumerator RandomlySpawnGumballsInBackground()
+        {
+            float timer = gumballSpawnDelay;
+            while (true)
+            {
+                if (timer >= gumballSpawnDelay)
+                {
+                    RandomlySpawnGumball();
+                    timer = 0f;
+                }
+                LowerRandomlySpawnedGumballs();
+                DeleteOffScreenRandomlySpawnedGumballs();
+                timer += Time.deltaTime;
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+        }
+
+        private void RandomlySpawnGumball()
+        {
+            int chosenGumballSpriteIndex =
+                Mathf.RoundToInt(Random.Range(0f, gumballSprites.Count - 1));
+            Sprite chosenGumballSprite = gumballSprites[chosenGumballSpriteIndex];
+            Vector2 chosenGumballPosition = new Vector2(
+                Random.Range(spawnedGumballStartX, spawnedGumballEndX),
+                spawnedGumballStartY
+            );
+            GameObject randomlySpawnedGumball = Instantiate(guiGumballPrefab, gumballContainer.transform);
+            randomlySpawnedGumball.transform.localPosition = chosenGumballPosition;
+            randomlySpawnedGumball.GetComponent<Image>().sprite = chosenGumballSprite;
+        }
+
+        private void LowerRandomlySpawnedGumballs()
+        {
+            foreach (Transform childTransform in gumballContainer.transform)
+            {
+                childTransform.Translate(Vector2.down * Time.deltaTime * gumballFallSpeed);
+            }
+        }
+
+        private void DeleteOffScreenRandomlySpawnedGumballs()
+        {
+            for (int i = gumballContainer.transform.childCount - 1; i >= 0; i--)
+            {
+                Transform randomlySpawnedGumballTransform =
+                    gumballContainer.transform.GetChild(i).transform;
+                bool randomlySpawnedGumballIsOffScreen =
+                    randomlySpawnedGumballTransform.localPosition.y < spawnedGumballEndY;
+                if (randomlySpawnedGumballIsOffScreen)
+                {
+                    Destroy(randomlySpawnedGumballTransform.gameObject);
+                }
+            }
         }
 
         private void ResetMaxAndQuinnTransforms()
@@ -46,6 +123,7 @@ namespace GUI.MainMenu
                 quinnTransformStartY
             );
         }
+
 
         private IEnumerator RaiseMaxAndQuinnTransforms()
         {
