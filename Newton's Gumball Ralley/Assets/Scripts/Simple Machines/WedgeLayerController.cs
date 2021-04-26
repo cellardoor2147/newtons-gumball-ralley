aead50c0425ceeceb9cfc9a8dfee6b0cd0bb56c5
Michael Renda <micoo227@users.noreply.github.com>
2021-04-26 19:35:07 -0400

    Further wedge fixes

--- Newton's Gumball Ralley/Assets/Scripts/Simple Machines/WedgeLayerController.cs
+++ Newton's Gumball Ralley/Assets/Scripts/Simple Machines/WedgeLayerController.cs
@@ -24,7 +24,7 @@ namespace Wedge
                 targetWedge.layer = defaultLayer;
         }
 
-        private void OnTriggerStay2D(Collider2D collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             DestructibleObstacleLayerController destructible
                 = collision.GetComponentInParent<DestructibleObstacleLayerController>();
