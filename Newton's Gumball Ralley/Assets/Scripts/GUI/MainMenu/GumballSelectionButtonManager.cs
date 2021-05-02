3acee24e6e815a640d89b22589d536eb9e7401b1
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-05-02 16:03:45 -0700

    Added gumball color selection screen

---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/MainMenu/GumballSelectionButtonManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Core;
+using Core.Levels;
+using TMPro;
+
+namespace GUI.MainMenu
+{
+    public class GumballSelectionButtonManager : MonoBehaviour
+    {
+        private static readonly string BUTTON_KEY = "Button";
+        private static readonly string STAR_REQUIREMENTS_KEY = "Star Requirement";
+        private static readonly string TEXT_KEY = "Text (TMP)";
+
+        [SerializeField] private int starRequirement;
+        [SerializeField] private Sprite unlockedSprite;
+
+        private void OnEnable()
+        {
+            SetStarRequirementText();
+            SetButton();
+        }
+
+        private void SetStarRequirementText()
+        {
+            transform.Find(STAR_REQUIREMENTS_KEY)
+                .Find(TEXT_KEY)
+                .GetComponent<TextMeshProUGUI>()
+                .text = starRequirement.ToString();
+        }
+
+        private void SetButton()
+        {
+            bool shouldBeClickable =
+                starRequirement <= PlayerProgressManager.GetTotalEarnedStars();
+            transform.Find(BUTTON_KEY)
+                .GetComponent<Image>()
+                .sprite = unlockedSprite;
+            transform.Find(BUTTON_KEY)
+                .GetComponent<Button>()
+                .interactable = shouldBeClickable;
+        }
+
+        public void SetGumballColor()
+        {
+            GameStateManager.SetGumballSprite(unlockedSprite);
+        }
+    }
+}
---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/MainMenu/GumballSelectionManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUI.MainMenu
+{
+    public class GumballSelectionManager : MonoBehaviour
+    {
+        private static readonly string SHOW_GUMBALLS_LEFT_BUTTON_KEY =
+            "Show Gumballs Left Button";
+        private static readonly string SHOW_GUMBALLS_RIGHT_BUTTON_KEY =
+            "Show Gumballs Right Button";
+        private static readonly string BUTTONS_FOR_WORLDS_PREFIX =
+            "Buttons for Gumballs ";
+        private static readonly string GUMBALLS_1_TO_6_SUFFIX =
+            "1-6";
+        private static readonly string GUMBALLS_7_TO_12_SUFFIX =
+            "7-12";
+        private static readonly string GUMBALLS_13_TO_18_SUFFIX =
+            "13-18";
+
+        private Button showGumballsLeftButton;
+        private Button showGumballsRightButton;
+        private GameObject buttonsForGumballs1To6;
+        private GameObject buttonsForGumballs7To12;
+        private GameObject buttonsForGumballs13To18;
+
+        private void Awake()
+        {
+            /* TODO: readd gumball left/right buttons once there are more colors to choose from
+            showGumballsLeftButton =
+                transform.Find(SHOW_GUMBALLS_LEFT_BUTTON_KEY).gameObject.GetComponent<Button>();
+            showGumballsRightButton =
+                transform.Find(SHOW_GUMBALLS_RIGHT_BUTTON_KEY).gameObject.GetComponent<Button>();
+            */
+            buttonsForGumballs1To6 =
+                transform.Find(BUTTONS_FOR_WORLDS_PREFIX + GUMBALLS_1_TO_6_SUFFIX).gameObject;
+            buttonsForGumballs7To12 =
+                transform.Find(BUTTONS_FOR_WORLDS_PREFIX + GUMBALLS_7_TO_12_SUFFIX).gameObject;
+            buttonsForGumballs13To18 =
+                transform.Find(BUTTONS_FOR_WORLDS_PREFIX + GUMBALLS_13_TO_18_SUFFIX).gameObject;
+        }
+
+        public void ShowGumballsLeft()
+        {
+            if (buttonsForGumballs7To12.activeSelf)
+            {
+                SetAllButtonsForGumballsContainersToInactive();
+                buttonsForGumballs1To6.SetActive(true);
+                showGumballsLeftButton.interactable = false;
+            }
+            else if (buttonsForGumballs13To18.activeSelf)
+            {
+                SetAllButtonsForGumballsContainersToInactive();
+                buttonsForGumballs7To12.SetActive(true);
+                showGumballsRightButton.interactable = true;
+            }
+        }
+
+        public void ShowGumballsRight()
+        {
+            if (buttonsForGumballs1To6.activeSelf)
+            {
+                SetAllButtonsForGumballsContainersToInactive();
+                buttonsForGumballs7To12.SetActive(true);
+                showGumballsLeftButton.interactable = true;
+            }
+            else if (buttonsForGumballs7To12.activeSelf)
+            {
+                SetAllButtonsForGumballsContainersToInactive();
+                buttonsForGumballs13To18.SetActive(true);
+                showGumballsRightButton.interactable = false;
+            }
+        }
+
+        private void SetAllButtonsForGumballsContainersToInactive()
+        {
+            buttonsForGumballs1To6.SetActive(false);
+            buttonsForGumballs7To12.SetActive(false);
+            buttonsForGumballs13To18.SetActive(false);
+        }
+    }
+}
