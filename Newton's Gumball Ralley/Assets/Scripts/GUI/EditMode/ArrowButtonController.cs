8c4325eb657eb0ae3840d416334054533bd614bc
Joshua Reed <43630084+cellardoor2147@users.noreply.github.com>
2021-05-06 13:43:56 -0700

    Added button to edit mode toolbar to lower/raise it

---
+++ Newton's Gumball Ralley/Assets/Scripts/GUI/EditMode/ArrowButtonController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GUI.EditMode
+{
+    public enum ToolbarArrowDirection
+    {
+        Down = 0,
+        Up = 1
+    }
+
+    public class ArrowButtonController : MonoBehaviour, IPointerClickHandler
+    {
+        private RectTransform rectTransform;
+        private ToolbarArrowDirection arrowDirection;
+        private Vector3 originalAnchoredPosition;
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+        }
+
+        private void OnEnable()
+        {
+            EditModeManager.ShowEditModeGUI();
+            arrowDirection = ToolbarArrowDirection.Down;
+            SetArrowRotation();
+            SetArrowPosition();
+        }
+
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (arrowDirection.Equals(ToolbarArrowDirection.Down))
+            {
+                EditModeManager.HideEditModeGUI();
+                arrowDirection = ToolbarArrowDirection.Up;
+            }
+            else
+            {
+                EditModeManager.ShowEditModeGUI();
+                arrowDirection = ToolbarArrowDirection.Down;
+            }
+            SetArrowRotation();
+            SetArrowPosition();
+        }
+
+        private void SetArrowRotation()
+        {
+            rectTransform.localEulerAngles = arrowDirection.Equals(ToolbarArrowDirection.Down)
+                ? new Vector3(0f, 0f, 0f)
+                : new Vector3(0f, 0f, 180f);
+        }
+
+        private void SetArrowPosition()
+        {
+            rectTransform.anchoredPosition = arrowDirection.Equals(ToolbarArrowDirection.Down)
+                ? originalAnchoredPosition
+                : originalAnchoredPosition + Vector3.up * 100f;
+        }
+    }
+}
